f5b803b6ad8f51ad79eb31812aa166ab0c982cb9
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-12-21 15:05:01 -0600

    refactor(navbar): remove unnecesary logs

--- src/application/ui/components/Navbar.jsx
+++ src/application/ui/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { ListItem } from './ListItem';
 export const NavBar = () => {
     const { user } = useAuthStore();
     const { showNavbar } = useUIStore();
-    console.log(showNavbar);
+
     return (
         <aside className={`min-w-[13rem] bg-gradient-to-br from-blue-500 to-cyan-500 font-bold text-white text-sm overflow-y-auto ${!showNavbar ? "animate-fade-out" : "animate-fade-in"}`}>
             <ul className="p-3 space-y-3">
