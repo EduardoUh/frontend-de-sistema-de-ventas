24937c22a4c8044e23ac7e17d70a06473a546e9e
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-12-23 15:16:41 -0600

    feat(brands): start brands module implementation

--- src/application/pages/Brands.jsx
+++ src/application/pages/Brands.jsx
@@ -1,7 +1,102 @@
-import React from 'react'
+import { useForm } from '../../hooks';
+import { InputComponent } from '../../utilities';
 
-export const Brands = () => {
+
+const filtersForm = {
+    nombre: '',
+    ciudad: '',
+    email: '',
+    activa: '',
+    creador: ''
+}
+
+const selectOptions = [
+    {
+        value: "true",
+        text: 'Activa'
+    },
+    {
+        value: "false",
+        text: 'Inactiva'
+    }
+]
+
+// Permissions in this module -> CREAR - VER -ACTUALIZAR
+export const Brands = ({ permissions }) => {
+    const { nombre, ciudad, email, activa, creador, handleInputChange, isFormSubmitted, setFormSubmitted, handleResetForm } = useForm(filtersForm);
+
+    if (!permissions || !Array.isArray(permissions) || Array.isArray(permissions) && permissions.length === 0) return (<div>Sin credenciales en &eacute;ste m&oacute;dulo</div>)
+
     return (
-        <div>Brands</div>
+        <div className="space-y-3">
+            <h2 className="text-center font-bold text-lg">Sucursales</h2>
+            {
+                permissions.find(permission => permission === 'CREAR') &&
+                (
+                    <div className="p-2">
+                        <button className="rounded-3xl px-3 py-1 bg-gradient-to-br from-blue-500  to-purple-300 text-white font-bold text-3xl flex justify-center items-center transition hover:scale-110 hover:bg-indigo-700">
+                            +
+                        </button>
+                    </div>
+                )
+            }
+            {
+                permissions.find(permission => permission === 'VER') &&
+                (
+                    // TODO: Implement the following filters -> nombre, ciudad, email, activa, creador(id)
+                    <>
+                        <div className="border">
+                            <form className="border border-black">
+                                <div className="flex flex-wrap justify-around">
+                                    <InputComponent
+                                        inputId='nombre'
+                                        inputName='nombre'
+                                        inputType='text'
+                                        placeholder='Nombre de la sucursal'
+                                        labelText='Filtrar por nombre'
+                                        handleChange={handleInputChange}
+                                        value={nombre}
+                                        containerStyle='border border-red-400 w-[30%]'
+                                    />
+                                    <InputComponent
+                                        inputId='ciudad'
+                                        inputName='ciudad'
+                                        inputType='text'
+                                        placeholder='Nombre de la ciudad'
+                                        labelText='Filtrar por ciudad'
+                                        handleChange={handleInputChange}
+                                        value={ciudad}
+                                        containerStyle='border border-red-400 w-[30%]'
+                                    />
+                                    <InputComponent
+                                        inputId='email'
+                                        inputName='email'
+                                        inputType='text'
+                                        placeholder='Email'
+                                        labelText='Filtrar por email'
+                                        handleChange={handleInputChange}
+                                        value={email}
+                                        containerStyle='border border-red-400 w-[30%]'
+                                    />
+                                    <InputComponent
+                                        inputId='activa'
+                                        inputName='activa'
+                                        inputType='select'
+                                        labelText='Filtrar status'
+                                        handleChange={handleInputChange}
+                                        value={activa}
+                                        selectOptions={selectOptions}
+                                        containerStyle='border border-red-400 w-[30%]'
+                                    />
+                                </div>
+                            </form>
+                        </div>
+                        <div className="border">
+                            Cards and pagination here
+                        </div>
+                    </>
+                )
+            }
+        </div >
     )
 }
--- src/application/routes/ApplicationRoutes.jsx
+++ src/application/routes/ApplicationRoutes.jsx
@@ -16,10 +16,10 @@ export const ApplicationRoutes = () => {
                 <section className={`${showNavbar ? "w-6/12 sm:w-10/12" : "w-full"} border-2 border-black p-2 overflow-auto`}>
                     <Routes>
                         <Route path="/home" element={<Home />} />
-                        <Route path="*" element={<Navigate to="/home" />} />
+                        <Route path="*" element={<Navigate to="/sucursales" />} /> {/* //TODO: change the default route */}
                         {
                             modulesComponentsCollection.map((Component, index) => (
-                                < Route key={modulesCollection[index].nombre} path={modulesCollection[index].ruta} element={< Component />} />
+                                < Route key={modulesCollection[index]?.nombre} path={modulesCollection[index]?.ruta} element={<Component permissions={modulesCollection[index]?.permisos} />} />
                             ))
                         }
                     </Routes>
