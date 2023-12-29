a1541878a7f5757d438df2fbc30f08440658cf78
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2023-12-29 14:41:37 -0600

    refactor(branches): change the opacity property for bg-opacity so content within modal can be displayed without opacity

--- src/application/ui/components/Modal.jsx
+++ src/application/ui/components/Modal.jsx
@@ -1,7 +1,7 @@
 export const Modal = ({ children, showModal = false }) => {
     if (!showModal) return (<></>);
     return (
-        <div className="fixed w-full h-screen bg-black bottom-0 left-0 opacity-75 flex justify-center items-center">
+        <div className="fixed w-full h-screen bg-black bottom-0 left-0 bg-opacity-75 flex justify-center items-center">
             {children}
         </div>
     )
