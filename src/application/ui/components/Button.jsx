4a7f26eae99e0d64a638aa201d1e2a0c7fe9fa3d
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2024-01-09 11:55:08 -0600

    fix(branches): add parenthesis to the conditional return to avoid potential bugs

--- src/application/ui/components/Button.jsx
+++ src/application/ui/components/Button.jsx
@@ -10,20 +10,24 @@ export const Button = ({ text = '', handleClick = null, type = 'submit', buttonS
         <>
             {
                 handleClick !== null ?
-                    <button
-                        className={`rounded-md bg-gradient-to-br from-blue-500  to-purple-300 p-2 text-white font-bold ${buttonSyles} hover:from-blue-700 hover:to-purple-700 active:from-blue-300 active:to-purple-300`}
-                        onClick={handleClick}
-                        type={buttonTypes[type] ? buttonTypes[type] : buttonTypes.button}
-                    >
-                        {text ? text : 'Default text'}
-                    </button>
+                    (
+                        <button
+                            className={`rounded-md bg-gradient-to-br from-blue-500  to-purple-300 p-2 text-white font-bold ${buttonSyles} hover:from-blue-700 hover:to-purple-700 active:from-blue-300 active:to-purple-300`}
+                            onClick={handleClick}
+                            type={buttonTypes[type] ? buttonTypes[type] : buttonTypes.button}
+                        >
+                            {text ? text : 'Default text'}
+                        </button>
+                    )
                     :
-                    <button
-                        className={`rounded-md bg-gradient-to-br from-blue-500  to-purple-300 p-2 text-white font-bold ${buttonSyles} hover:from-blue-700 hover:to-purple-700 active:from-blue-300 active:to-purple-300`}
-                        type={buttonTypes[type] ? buttonTypes[type] : buttonTypes.button}
-                    >
-                        {text ? text : 'Default text'}
-                    </button>
+                    (
+                        <button
+                            className={`rounded-md bg-gradient-to-br from-blue-500  to-purple-300 p-2 text-white font-bold ${buttonSyles} hover:from-blue-700 hover:to-purple-700 active:from-blue-300 active:to-purple-300`}
+                            type={buttonTypes[type] ? buttonTypes[type] : buttonTypes.button}
+                        >
+                            {text ? text : 'Default text'}
+                        </button>
+                    )
             }
         </>
     )
