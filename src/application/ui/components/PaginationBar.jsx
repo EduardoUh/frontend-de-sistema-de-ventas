90efb6b68c19ed2a379fe560c5139a3645fc9efb
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-01-10 13:31:18 -0600

    feat(PaginationBar): add the isLoading property to the className ternary operator

--- src/application/ui/components/PaginationBar.jsx
+++ src/application/ui/components/PaginationBar.jsx
@@ -2,23 +2,23 @@ export const PaginationBar = ({ pagesCanBeGenerated = null, currentPage = null, 
     if (!pagesCanBeGenerated || !currentPage || !nextPage || !previousPage) return (<div>All parameters are required</div>)
     return (
         <div className="space-y-3 mt-5">
-            <div className="text-center">
-                {`Página ${currentPage} de ${pagesCanBeGenerated}`}
-            </div>
-            <div className="flex justify-center space-x-7">
-                <button 
-                className={`font-bold w-12 rounded-md text-white bg-gradient-to-br from-blue-500 to-purple-300 ${currentPage === 1 ? "from-gray-300 to-gray-100 text-gray-300" : ""}`} 
-                onClick={previousPage} 
-                disabled={currentPage === 1 || isLoading}
-                type="button"
+
+            <div className="flex justify-center space-x-2">
+                <button
+                    className={`font-bold w-12 rounded-md text-white bg-gradient-to-br from-blue-500 to-purple-300 ${currentPage === 1 || isLoading ? "from-gray-300 to-gray-100 text-gray-300" : ""}`}
+                    onClick={previousPage}
+                    disabled={currentPage === 1 || isLoading}
+                    type="button"
                 >
                     &lt;
                 </button>
-
-                <button className={`font-bold w-12 rounded-md text-white bg-gradient-to-br from-blue-500 to-purple-300 ${currentPage === pagesCanBeGenerated ? "from-gray-300 to-gray-100 text-gray-300" : ""}`} 
-                onClick={nextPage} 
-                disabled={currentPage === pagesCanBeGenerated || isLoading}
-                type="button"
+                <div className="text-center">
+                    {`Página ${currentPage} de ${pagesCanBeGenerated}`}
+                </div>
+                <button className={`font-bold w-12 rounded-md text-white bg-gradient-to-br from-blue-500 to-purple-300 ${currentPage === pagesCanBeGenerated || isLoading ? "from-gray-300 to-gray-100 text-gray-300" : ""}`}
+                    onClick={nextPage}
+                    disabled={currentPage === pagesCanBeGenerated || isLoading}
+                    type="button"
                 >
                     &gt;
                 </button>
