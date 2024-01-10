b6e860109d61a246146f87d391a2b9ea3f5b6f52
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-01-10 14:17:37 -0600

    feat(branches): finish creating and implementing PaginatedSelect component

--- src/application/ui/components/PaginatedSelect.jsx
+++ src/application/ui/components/PaginatedSelect.jsx
@@ -1,6 +1,7 @@
+import { useEffect } from 'react';
 import { usePagination } from '../../../hooks';
 import { Message } from '../../../utilities';
-import { PaginationBar, PaginationContainer } from '../';
+import { PaginationBar } from '../';
 
 
 const formatDataToBeUsedInPaginatedSelector = (keyToGetSelectValue = '', keyToGetSelectText = '', data = []) => {
@@ -17,6 +18,10 @@ const formatDataToBeUsedInPaginatedSelector = (keyToGetSelectValue = '', keyToGe
 export const PaginatedSelect = ({ baseUrl, keyToGetData = '', inputId = 'defaultSelectId', inputName = 'defaultSelectName', value = 'defaultSelectValue', handleChange = null, hasError = false, errorMessage = 'Error in the paginated input', severity = 'error', containerStyle = '', labelStyle = '', inputStyle = '', labelText = '', keyToGetSelectValue = '', keyToGetSelectText = '' }) => {
     const { data, page, error, isLoading, nextPage, previousPage } = usePagination(baseUrl);
 
+    useEffect(() => {
+        if (isLoading && handleChange && handleChange instanceof Function) handleChange({ target: { name: inputName, value: '' } });
+    }, [isLoading]);
+
     if (!handleChange || !(handleChange instanceof Function)) return (<Message message='handleChange function is required' severity='error' />);
 
     if (error.hasError || !data) return (<></>);
@@ -24,11 +29,15 @@ export const PaginatedSelect = ({ baseUrl, keyToGetData = '', inputId = 'default
     return (
         <>
             <div className={`flex flex-col space-y-2 ${containerStyle.toLowerCase()}`}>
-                <label htmlFor={inputId} className={`font-semibold ${labelStyle.toLowerCase()}`}>{labelText}: </label>
+                <label htmlFor={inputId} className={`font-semibold ${labelStyle.toLowerCase()}`}>{labelText}:</label>
+                {
+                    data?.pagesCanBeGenerated > 1 &&
+                    <PaginationBar currentPage={page} pagesCanBeGenerated={data.pagesCanBeGenerated} isLoading={isLoading} nextPage={nextPage} previousPage={previousPage} />
+                }
                 <select id={inputId} name={inputName} value={value} onChange={handleChange} className={`rounded-md text-center border-2 border-gray-300 ${inputStyle.toLowerCase()}`}>
                     <option value="">--Seleccionar--</option>
                     {
-                        data ?
+                        data && !isLoading ?
                             formatDataToBeUsedInPaginatedSelector(keyToGetSelectValue, keyToGetSelectText, data[keyToGetData])?.map(option => (
                                 <option key={option.value} value={option.value}>{option.text}</option>
                             ))
