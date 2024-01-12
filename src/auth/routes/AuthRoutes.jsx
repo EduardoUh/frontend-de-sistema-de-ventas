123316188bf164d0e3e137084b5381c77df7bc80
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2024-01-12 13:03:19 -0600

    refactor(auth): add extra checking to always redirect to auth/login route

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -5,7 +5,8 @@ import { Login } from '../pages';
 export const AuthRoutes = () => {
     return (
         <Routes>
-            <Route path="/auth/*" element={<Login />} />
+            <Route path='/auth/login' element={<Login />} />
+            <Route path="/auth/*" element={<Navigate to='/login' />} />
             <Route path="/*" element={<Navigate to="/auth/login" />} />
         </Routes>
     )
