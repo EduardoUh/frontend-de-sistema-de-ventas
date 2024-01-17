1a31802e5c1fecb498cffa312429a8836b61c5d2
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2024-01-17 15:38:19 -0600

    feat(store): add feature to update the user info to the authSlice

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -4,6 +4,7 @@ export * from './useUIStore.js';
 export * from './useRecordsStore.Pagination.js';
 export * from './useRecordsStore.Update.js';
 export * from './useRecordsStore.Create.js';
+export * from './useAuthStore.UpdateProfile.js'
 // ? normal custom hooks
 export * from './useForm.js';
 export * from './useModules.js';
