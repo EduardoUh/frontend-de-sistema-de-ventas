e7aa765740760f783fd6505805a27d989f6983e4
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2024-01-26 12:04:41 -0600

    feat(customers): implement logic and components to create a new customer

--- src/application/pages/Customers.jsx
+++ src/application/pages/Customers.jsx
@@ -1,5 +1,5 @@
 import { useRecordsStorePaginationHooks, useUIStore } from '../../hooks';
-import { CustomersPagination, CustomersFilters, CustomersUpdateForm } from '../components/customers';
+import { CustomersPagination, CustomersFilters, CustomersUpdateForm, BranchesCreateButton, CustomersCreateForm } from '../components/customers';
 
 
 const baseUrl = '/clientes';
@@ -8,7 +8,7 @@ const ketToGetData = 'clientes';
 
 export const Customers = ({ permissions, name }) => {
     useRecordsStorePaginationHooks(name, baseUrl, ketToGetData);
-    const { updateModalIsOpen } = useUIStore();
+    const { createModalIsOpen, updateModalIsOpen } = useUIStore();
 
     if (!permissions || !Array.isArray(permissions) || Array.isArray(permissions) && permissions.length === 0) return (<div className='text-center font-bold text-3xl'>Sin credenciales en &eacute;ste m&oacute;dulo</div>)
 
@@ -16,11 +16,18 @@ export const Customers = ({ permissions, name }) => {
         <div className='space-y-3'>
             <h2 className='text-center font-bold text-xl'>{name}</h2>
             {
+                permissions.find(permission => permission === 'CREAR') &&
+                <BranchesCreateButton />
+            }
+            {
                 permissions.find(permission => permission === 'VER') &&
                 <>
                     <CustomersFilters baseUrl={baseUrl} />
                     <CustomersPagination permissions={permissions} />
                 </>
+            }
+            {
+                createModalIsOpen && <CustomersCreateForm baseUrl={baseUrl} />
             }
             {
                 updateModalIsOpen && <CustomersUpdateForm baseUrl={baseUrl} />
