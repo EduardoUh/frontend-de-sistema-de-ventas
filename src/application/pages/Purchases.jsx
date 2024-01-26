f6a18e0610fd0031ff4f8bd953b1aee510a8a97e
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2024-01-26 15:32:16 -0600

    feat(purchases): implement pagination logic

--- src/application/pages/Purchases.jsx
+++ src/application/pages/Purchases.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useRecordsStorePaginationHooks, useUIStore } from '../../hooks';
+import { PurchasesPagination } from '../components/purchases';
+import { PurchasesDisplayItems } from '../components/purchases/PurchasesDisplayItems';
 
-export const Purchases = () => {
+
+const baseUrl = '/compras';
+
+const keyToGetData = 'compras';
+
+export const Purchases = ({ permissions, name }) => {
+    useRecordsStorePaginationHooks(name, baseUrl, keyToGetData);
+    const { showMoreModalIsOpen } = useUIStore();
+
+    if (!permissions || !Array.isArray(permissions) || Array.isArray(permissions) && permissions.length === 0) return (<div className='text-center font-bold text-3xl'>Sin credenciales en &eacute;ste m&oacute;dulo</div>)
+
     return (
-        <div>Purchases</div>
+        <div className='space-y-3'>
+            <h2 className='text-center font-bold text-xl'>{name}</h2>
+            {
+                permissions.find(permission => permission === 'VER') &&
+                <PurchasesPagination />
+            }
+            {
+                showMoreModalIsOpen && <PurchasesDisplayItems />
+            }
+        </div>
     )
 }
