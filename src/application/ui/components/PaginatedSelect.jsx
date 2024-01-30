6ace8ae8adaeef2069afe4241e70cbade3ff5a55
Eduardo Uh <eduardoivanuhgamino2@gmail.com>
2024-01-30 15:22:49 -0600

    refactor(paginatedSelect): modify to display a message if the isOptional prop is true

--- src/application/ui/components/PaginatedSelect.jsx
+++ src/application/ui/components/PaginatedSelect.jsx
@@ -14,7 +14,7 @@ const formatDataToBeUsedInPaginatedSelector = (keyToGetSelectValue = '', keyToGe
     );
 }
 
-export const PaginatedSelect = ({ baseUrl, keyToGetData = '', inputId = 'defaultSelectId', inputName = 'defaultSelectName', value = 'defaultSelectValue', handleChange = null, hasError = false, errorMessage = 'Error in the paginated input', severity = 'error', containerStyle = '', labelStyle = '', inputStyle = '', labelText = '', keyToGetSelectValue = '', keyToGetSelectText = '' }) => {
+export const PaginatedSelect = ({ baseUrl, keyToGetData = '', inputId = 'defaultSelectId', inputName = 'defaultSelectName', value = 'defaultSelectValue', handleChange = null, hasError = false, errorMessage = 'Error in the paginated input', severity = 'error', containerStyle = '', labelStyle = '', inputStyle = '', labelText = '', keyToGetSelectValue = '', keyToGetSelectText = '', isOptional = false }) => {
     const { data, page, error, isLoading, nextPage, previousPage } = usePagination(baseUrl);
 
     if (!handleChange || !(handleChange instanceof Function)) return (<Message message='handleChange function is required' severity='error' />);
@@ -28,6 +28,9 @@ export const PaginatedSelect = ({ baseUrl, keyToGetData = '', inputId = 'default
                 {
                     data?.pagesCanBeGenerated > 1 &&
                     <PaginationBar currentPage={page} pagesCanBeGenerated={data.pagesCanBeGenerated} isLoading={isLoading} nextPage={nextPage} previousPage={previousPage} />
+                }
+                {
+                    isOptional && <small className='text-blue-500'>El campo {labelText} es opcional</small>
                 }
                 <select id={inputId} name={inputName} value={value} onChange={handleChange} className={`rounded-md text-center border-2 border-gray-300 ${inputStyle.toLowerCase()}`}>
                     <option value="">--Seleccionar--</option>
