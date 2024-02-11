edc850c6a7b20a3379aac02b72b6ae8a0f066274
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-02-11 12:53:13 -0600

    feat(createSelling): implement components to show errors, error and success message if there is any

--- src/application/components/createSelling/CreateSellingShoppingCartAndPaymentPanel.jsx
+++ src/application/components/createSelling/CreateSellingShoppingCartAndPaymentPanel.jsx
@@ -5,12 +5,16 @@ import { ShowErrorMessages, ShowErrorMessage, ShowSuccessMessage } from '../../u
 
 
 export const CreateSellingShoppingCartAndPaymentPanel = () => {
-    const { error } = useCreateSellingStore();
+    const { error, errors, successMessage } = useCreateSellingStore();
 
     return (
         <div className='border shadow-md rounded-lg'>
             <h3 className='text-center font-semibold text-2xl'>Canasta</h3>
-            <ShowErrorMessage hasError={error.hasError} error={error.message} />
+            <div className="w-5/6">
+                <ShowErrorMessages hasErrors={errors.hasErrors} errors={errors.messages} />
+                <ShowErrorMessage hasError={error.hasError} error={error.message} />
+                <ShowSuccessMessage successMessage={successMessage} />
+            </div>
             <div className='flex flex-col md:flex-row md:justify-between p-2'>
                 <CreateSellingShoppingCart />
                 <CreateSellingPaymentPanel />
