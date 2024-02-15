f972579c81e9885d70aaab1bc331f59e503b263e
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-02-15 15:32:02 -0600

    feat(hooks): create hook to handle the createPurchaseSlice

---
+++ src/hooks/useCreatePurchaseStore.js
@@ -0,0 +1,42 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+    onSetBranch,
+    onSetProvider,
+    onClearPayloadExceptBranchAndProvider,
+} from '../store/records/createPurchaseSlice';
+
+
+export const useCreatePurchaseStore = () => {
+    const { payload, isLoading, errors, error, successMessage } = useSelector(state => state.createPurchase);
+    const dispatch = useDispatch();
+
+    const startSettingBranch = (branchId) => {
+        dispatch(onSetBranch(branchId));
+    }
+
+    const startSettingProvider = (providerId) => {
+        dispatch(onSetProvider(providerId));
+    }
+
+    const startClearPayloadExceptBranchAndProvider = () => {
+        dispatch(onClearPayloadExceptBranchAndProvider());
+    }
+
+    return {
+        // ? Properties
+        payload,
+        isLoading,
+        errors,
+        error,
+        successMessage,
+        //...payload,
+        sucursal: payload.sucursal,
+        proveedor: payload.proveedor,
+        articulos: payload.articulos,
+        total: payload.total,
+        // ? Methods
+        startSettingBranch,
+        startSettingProvider,
+        startClearPayloadExceptBranchAndProvider,
+    }
+}
