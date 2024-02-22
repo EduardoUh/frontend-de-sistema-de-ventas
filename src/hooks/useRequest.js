553d2d39ac337fa4534155de7273d550294fb2be
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-02-22 15:57:47 -0600

    feat(useRequest): add function to send a post request

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -7,10 +7,16 @@ const errorInitialForm = {
     errorMessage: null
 }
 
+const errorsInitialForm = {
+    hasErrors: false,
+    errorMessages: null
+}
+
 export const useRequest = () => {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(errorInitialForm);
+    const [errors, setErrors] = useState(errorsInitialForm);
 
     const requestData = async (url) => {
         try {
@@ -30,12 +36,33 @@ export const useRequest = () => {
         }
     }
 
+    const postData = async (url, payload) => {
+        try {
+            error.hasError && setError(errorInitialForm);
+            errors.hasErrors && setErrors(errorsInitialForm);
+            setIsLoading(true);
+            const { data } = await api.post(url, payload);
+            setData(data);
+
+        } catch (error) {
+            if (error.response.data.message) setError({ hasError: true, errorMessage: error.response.data.message });
+
+            if (error.response.data.errors) setErrors({ hasErrors: true, errorMessages: error.response.data.errors });
+
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
     return {
         // ?? Properties
         data,
         isLoading,
         error,
+        errors,
         // ?? Methods
-        requestData
+        requestData,
+        postData,
     }
 }
