0cf9324f06ddd31eb939d0cc9c96d238f342b9d8
Eduardo <eduardoivanuhgamino2@gmail.com>
2024-02-25 12:41:56 -0600

    fix(recordsSlice): fix bug that was causing error message to not be displayed and was setting records value to null

--- src/application/ui/components/PaginationContainer.jsx
+++ src/application/ui/components/PaginationContainer.jsx
@@ -7,7 +7,7 @@ export const PaginationContainer = ({ children, data, isLoading, error, page, pa
 
     return (
         <>
-            {data !== null && error.hasError === false ?
+            {error.hasError === false && data.length > 0 ?
                 (
                     <>
                         {children}
@@ -16,7 +16,7 @@ export const PaginationContainer = ({ children, data, isLoading, error, page, pa
                 )
                 :
                 (
-                    error.hasError === true && data === null && <div className="flex justify-center items-center"><Message message={error.errorMessage} severity='error' messageStyle='sm:w-1/3' /></div>
+                    error.hasError === true && data.length < 1 && <div className="flex justify-center items-center"><Message message={error.errorMessage} severity='error' messageStyle='sm:w-1/3' /></div>
                 )
             }
         </>
